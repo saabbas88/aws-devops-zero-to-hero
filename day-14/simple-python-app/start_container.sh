0abec5f5a5c581461857b440a2a7b89bffb0d576
saabbas88 <85310420+saabbas88@users.noreply.github.com>
2024-09-12 10:42:30 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull saabbas88/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d 5000:5080 saabbas88/simple-python-flask-app
